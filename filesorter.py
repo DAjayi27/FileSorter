01befed4fd57194bbb926aae5b4c8d627e4427ae
DAjayi <dr716500@dal.ca>
2025-04-23 23:36:09 -0300

    File sorter V2 beta added, This commits adds a number of new features to the file sorter script.
    1) The file sorter new instead of looping through all the files in the downloads directory each time it's run, now only loops through only the file not already in the category folders.
    
    2) this new version also now includes a logging systems that more properly tracks errors and file and folder modifications and creations. I added this after noticing a problem with v1 where the script would constantly create a bunch of new category folders even though they were'nt needed. The logger should make debugging this issue much easier .
    
    3)  The script is now also able to cleanup emty folder that are left in the downloads directory. This should prevent too much clutter from just sitting there.

--- filesorter.py
+++ filesorter.py
@@ -3,44 +3,85 @@ import glob
 import shutil
 import re
 import uuid
+from pathlib import Path
+from logger import logCreation
+from tqdm import tqdm
+
+def is_folder_empty(folder_path):
+    return len(os.listdir(folder_path)) == 0
+
 
 download_dir = os.path.join(os.path.expanduser("~"), "Downloads/*")
-file_names = glob.glob(download_dir)
 
-# File extension lists
-documents = ['.pdf', '.docx', '.doc', '.txt', '.accdb', '.pptx', '.xls', '.csv']
-media = ['.jpeg', '.jpg', '.svg', '.png', '.PNG', '.mp4', '.mp3']
-setupFiles = ['.exe', '.msi', '.apk']
-compressedFiles = ['.zip']
+file_names = glob.glob(download_dir,recursive=True)
+
+
+# File extension lists for each category
+
+documents_ext = ['.pdf', '.doc', '.docx', '.txt', '.odt', '.xls', '.xlsx', '.ppt', '.pptx', '.csv']
+photos_ext = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
+music_ext = ['.mp3', '.wav', '.aac', '.flac', '.ogg', '.m4a']
+videos_ext = ['.mp4', '.mkv', '.mov', '.avi', '.wmv', '.flv']
+zip_files_ext = ['.zip', '.rar', '.7z', '.tar', '.gz']
+miscellaneous_ext = ['.exe', '.apk', '.iso', '.bak', '.tmp']
 
 # Destination folder locations
-Document_Location = 'C:/Users/daraa/Downloads/Documents'
-Media_Location = 'C:/Users/daraa/Downloads/Media'
-SetupFiles_Location = 'C:/Users/daraa/Downloads/SetupFiles'
-CompressedFiles_Location = 'C:/Users/daraa/Downloads/CompressedFiles'
-Misselaneous_Location = 'C:/Users/daraa/Downloads/Misselaneous'
+Document_Location = 'C:/Users/daraa/Downloads/Documents_2'
+Photos_Location = 'C:/Users/daraa/Downloads/Images_2'
+Video_Location = 'C:/Users/daraa/Downloads/Video_2'
+Audio_Location = 'C:/Users/daraa/Downloads/Audio_2'
+SetupFiles_Location = 'C:/Users/daraa/Downloads/SetupFiles_2'
+CompressedFiles_Location = 'C:/Users/daraa/Downloads/CompressedFiles_2'
+Misselaneous_Location = 'C:/Users/daraa/Downloads/Misselaneous_2'
 
-for file in file_names:
+
+excluded_folders = [Document_Location,Photos_Location,Video_Location,Audio_Location,SetupFiles_Location,CompressedFiles_Location,Misselaneous_Location]
+
+# Normalize excluded folder paths for accurate comparison
+excluded_folders = [os.path.abspath(folder) for folder in excluded_folders]
+
+
+
+for file in   tqdm(file_names, desc="Processing files") :
+    
+    if os.path.isdir(file) or any(os.path.commonpath([file, folder]) == folder for folder in excluded_folders) :
+        continue
+    
+    
     destination_folder = None
 
-    if os.path.splitext(file)[1] in documents:
+    if Path(file).suffix in documents_ext:
         destination_folder = Document_Location
-    elif os.path.splitext(file)[1] in media:
-        destination_folder = Media_Location
-    elif os.path.splitext(file)[1] in setupFiles:
-        destination_folder = SetupFiles_Location
-    elif os.path.splitext(file)[1] in compressedFiles:
-        destination_folder = CompressedFiles_Location
-    elif re.match(r"\.[^.]*$", os.path.splitext(file)[1]):
+    elif Path(file).suffix in photos_ext:
+        destination_folder = Photos_Location
+    elif Path(file).suffix in music_ext:
+        destination_folder = Audio_Location
+    elif Path(file).suffix in videos_ext:
+        destination_folder = Video_Location
+    elif Path(file).suffix in zip_files_ext:
+        destination_folder =  CompressedFiles_Location
+    else:
         destination_folder = Misselaneous_Location
 
     if destination_folder:
         destination_path = os.path.join(destination_folder, os.path.basename(file))
 
         # Handle file name collisions
-        while os.path.exists(destination_path):
+        if os.path.exists(destination_path):
             base_name, extension = os.path.splitext(destination_path)
             destination_path = f"{base_name}_{uuid.uuid4().hex[:8]}{extension}"
-
-        os.makedirs(destination_folder, exist_ok=True)
+        
+        if not os.path.isdir(destination_folder):
+            logCreation(f"The directory '{destination_folder}' was created by the program")
+            os.makedirs(destination_folder)
+        
         shutil.move(file, destination_path)
+
+    
+        
+# cleanup empty folders 
+
+for file in tqdm(file_names, desc="Deleting Empty Folders"):
+    if os.path.isdir(file) and is_folder_empty(file):
+        os.rmdir(file)
+        
